94242f263771547d5979a74b99b18a730cbb35f2
Karthik-beta <karthikpar@protonmail.com>
2022-12-26 14:43:49 +0530

    Revert "Update contact.js"
    This reverts commit 3210444fc21e95a2928370d795496cb2208efbd8.

--- mail/contact.js
+++ mail/contact.js
@@ -15,12 +15,12 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formsubmit.co/karthikpar@protonmail.com",
+                url: "contact.php",
                 type: "POST",
                 data: {
                     name: name,
-                    // email: email,
-                    // subject: subject,
+                    email: email,
+                    subject: subject,
                     message: message
                 },
                 cache: false,
