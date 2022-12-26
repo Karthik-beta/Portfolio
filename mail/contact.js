b87edcef9e02ccc98ebcda6637b79e0916b21685
Karthik-beta <karthikpar@protonmail.com>
2022-12-26 14:48:10 +0530

    1.5

--- mail/contact.js
+++ mail/contact.js
@@ -15,12 +15,12 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formsubmit.co/karthikpar@protonmail.com",
+                url: "https://formspree.io/f/mzbwnowb",
                 type: "POST",
                 data: {
                     name: name,
-                    email: email,
-                    subject: subject,
+                    // email: email,
+                    // subject: subject,
                     message: message
                 },
                 cache: false,
