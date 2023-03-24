246a15b1d82ed6b50d5b66635f173f23f8ad0bf7
Karthik-beta <karthikpar@protonmail.com>
2023-03-24 16:17:09 +0530

    update3

--- js/main.js
+++ js/main.js
@@ -78,14 +78,27 @@
 
 
     // Portfolio isotope and filter
+    // var portfolioIsotope = $('.portfolio-container').isotope({
+    //     itemSelector: '.portfolio-item',
+    //     layoutMode: 'fitRows'
+    // });
+    // $('#portfolio-flters li').on('click', function () {
+    //     $("#portfolio-flters li").removeClass('active');
+    //     $(this).addClass('active');
+
+    //     portfolioIsotope.isotope({filter: $(this).data('filter')});
+    // });
+    
+    // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        layoutMode: 'masonry'
     });
+    
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
-
+    
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
     
